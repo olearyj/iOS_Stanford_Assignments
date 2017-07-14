560057f08e656e4d92705539cf5f741ed220f6f6
James O'Leary <olearyj234@gmail.com>
2017-07-14 08:39:41 -0400

    Added more buttons and an action for all the digits

--- Project 1/Project 1/ViewController.swift
+++ Project 1/Project 1/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        print("Digit pressed: \(sender.currentTitle!)")
+        
+    }
 
 }
 
